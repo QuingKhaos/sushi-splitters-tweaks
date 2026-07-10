f3fc387df60362101ca25748e645834acd0df811
Em Karisch <emii.khaos@gmail.com>
2026-07-10 18:04:01 +0200

    feat: add splitter icon

---
+++ data-final-fixes.lua
@@ -0,0 +1,30 @@
+local khaoslib_entity = require("__khaoslib__.entity")
+local khaoslib_item = require("__khaoslib__.item")
+local khaoslib_recipe = require("__khaoslib__.recipe")
+
+-- Add hint which recipe is made from splitters
+local sushi_splitters = khaoslib_recipe.find(function (recipe)
+  return recipe.name:match("sushi") ~= nil and recipe.name:match("upgrade") == nil
+end)
+
+for _, recipe in pairs(sushi_splitters) do
+  local splitter = khaoslib_recipe.get_ingredient(recipe, function (ingredient)
+    return ingredient.name:match("splitter") ~= nil and ingredient.name:match("sushi") == nil
+  end)
+
+  if splitter then
+    local recipe_instance = khaoslib_recipe:load(recipe)
+    if recipe_instance:count_icons() == 0 then
+      recipe_instance:set_icons(khaoslib_item.get_icons(recipe_instance:get_result(function (result)
+        return result.name:match("sushi") ~= nil
+      end).name))
+    end
+
+    local icons = khaoslib_entity:load("splitter", data.raw["item"][splitter.name].place_result):get_icons()
+    if #icons > 0 then
+      recipe_instance:add_icon {icon = icons[1].icon, icon_size = icons[1].icon_size, scale = 0.25, shift = {-8, 8}}
+    end
+
+    recipe_instance:commit()
+  end
+end
