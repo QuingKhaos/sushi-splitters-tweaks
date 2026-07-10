d5ab0a918708cc8ab68e488df7918c8e7c630fcb
Em Karisch <emii.khaos@gmail.com>
2026-07-10 18:12:36 +0200

    feat: update entity icons

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -3,11 +3,11 @@ local khaoslib_item = require("__khaoslib__.item")
 local khaoslib_recipe = require("__khaoslib__.recipe")
 
 -- Add hint which recipe is made from splitters
-local sushi_splitters = khaoslib_recipe.find(function (recipe)
+local sushi_splitters_recipes = khaoslib_recipe.find(function (recipe)
   return recipe.name:match("sushi") ~= nil and recipe.name:match("upgrade") == nil
 end)
 
-for _, recipe in pairs(sushi_splitters) do
+for _, recipe in pairs(sushi_splitters_recipes) do
   local splitter = khaoslib_recipe.get_ingredient(recipe, function (ingredient)
     return ingredient.name:match("splitter") ~= nil and ingredient.name:match("sushi") == nil
   end)
@@ -27,4 +27,15 @@ for _, recipe in pairs(sushi_splitters) do
 
     recipe_instance:commit()
   end
+end
+
+-- Update entity icon to have the sushi splitter icon, not the splitter one
+local sushi_splitters_entities = khaoslib_entity.find("splitter", function (entity)
+  return entity.name:match("sushi") ~= nil
+end)
+
+for _, entity in pairs(sushi_splitters_entities) do
+  khaoslib_entity:load("splitter", entity)
+    :set_icons(khaoslib_item.get_icons(data.raw["splitter"][entity].minable.result))
+    :commit()
 end
